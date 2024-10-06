403b1896a436c117ab1229eba08a011ca5b6d0f9
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2024-10-05 20:03:57 -0400

    Add dropzone js

--- app/javascript/controllers/dropzone_controller.js
+++ app/javascript/controllers/dropzone_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
+import Dropzone from "dropzone"
+
+import { DirectUpload } from "@rails/activestorage"
+
+import { getMetaValue, findElement, removeElement, insertAfter } from "helpers"
+
 // Connects to data-controller="dropzone"
 export default class extends Controller {
   connect() {
+    console.log("Hello, Dropzone!")
   }
 }
